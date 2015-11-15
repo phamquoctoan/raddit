f080c0b95ca2ba953e260e752b64135d033992b8
phamquoctoan <phamquoctoan@hotmail.com>
2015-11-15 18:59:07 +0700

    Added name to User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :comments
 
   devise_for :users
   resources :links do
@@ -12,9 +11,6 @@ Rails.application.routes.draw do
   end
   root "links#index"
 
-  devise_scope :user do
-    get 'users/sign_out' => "devise/sessions#destroy"
-  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
